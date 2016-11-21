2f7291bb8426b1a106460c7176c035193da9b3e6
fwhp6126 <fwhp6126@mb.infoweb.ne.jp>
2016-11-21 17:37:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/owner/intent/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.owner.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.owner.intent.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
